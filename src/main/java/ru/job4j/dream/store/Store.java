b18fcc78a6cb3b0dd04abd4de93f55a1f2473674
Nefedova <Katerina163>
2021-12-13 10:17:24 +0400

    Scriplet. Динамическая таблица. [475130]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -2,26 +2,20 @@ package ru.job4j.dream.store;
 
 import ru.job4j.dream.model.Post;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Store {
 
     private static final Store INST = new Store();
-    private static final SimpleDateFormat simple_format = new SimpleDateFormat("dd.MM.yyyy");
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private Store() {
-        try {
-            posts.put(1, new Post(1, "Mark", "@mdo", simple_format.parse("01.02.2020")));
-            posts.put(2, new Post(2, "Jacob", "@fat", simple_format.parse("10.04.2020")));
-            posts.put(3, new Post(3, "Larry", "@twitter", simple_format.parse("12.07.2020")));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+            posts.put(1, new Post(1, "Mark", "@mdo", new GregorianCalendar(2021, 10, 10).getTime()));
+            posts.put(2, new Post(2, "Jacob", "@fat", new GregorianCalendar(2021, 01, 12).getTime()));
+            posts.put(3, new Post(3, "Larry", "@twitter", new GregorianCalendar(2021, 03, 8).getTime()));
     }
 
     public static Store instOf() {
