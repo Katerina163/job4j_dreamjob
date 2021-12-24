1e58794428a47ad60ba8e526b80e8a32df1f8666
Nefedova <Katerina163>
2021-12-24 15:05:18 +0400

    DbStore

--- src/main/java/ru/job4j/dream/servlet/DeleteServlet.java
+++ src/main/java/ru/job4j/dream/servlet/DeleteServlet.java
@@ -1,6 +1,6 @@
 package ru.job4j.dream.servlet;
 
-import ru.job4j.dream.store.Store;
+import ru.job4j.dream.store.MemStore;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +13,7 @@ public class DeleteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int id = Integer.parseInt(req.getParameter("id"));
-        boolean res = Store.instOf().deleteByIdCandidate(id);
+        boolean res = MemStore.instOf().deleteByIdCandidate(id);
         boolean rsl = new File("c:\\images\\" + id + ".jpg").delete();
         resp.sendRedirect(req.getContextPath() + "/candidates.do");
     }
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -4,62 +4,19 @@ import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Store {
-    private static final AtomicInteger POST_ID = new AtomicInteger(4);
-    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
-    private static final Store INST = new Store();
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+public interface Store {
+    Collection<Post> findAllPosts();
 
-    private Store() {
-        candidates.put(1, new Candidate(1, "Mark"));
-        candidates.put(2, new Candidate(2, "Jacob"));
-        candidates.put(3, new Candidate(3, "Larry"));
-        posts.put(1, new Post(1, "Junior Java"));
-        posts.put(2, new Post(2, "Middle Java"));
-        posts.put(3, new Post(3, "Senior Java"));
-    }
+    Collection<Candidate> findAllCandidates();
 
-    public static Store instOf() {
-        return INST;
-    }
+    void save(Post post);
 
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
+    Post findByIdPost(int id);
 
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
+    void save(Candidate candidate);
 
-    public void save(Post post) {
-        if (post.getId() == 0) {
-            post.setId(POST_ID.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
+    Candidate findByIdCandidate(int id);
 
-    public Post findByIdPost(int id) {
-        return posts.get(id);
-    }
 
-    public void save(Candidate candidate) {
-        if (candidate.getId() == 0) {
-            candidate.setId(CANDIDATE_ID.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
-
-    public Candidate findByIdCandidate(int id) {
-        return candidates.get(id);
-    }
-
-    public boolean deleteByIdCandidate(int id) {
-        Candidate candidate = candidates.remove(id);
-        return candidate != null;
-    }
 }
