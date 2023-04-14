248267b27d48e1d16fba65394e9711b42d05c3fb
Katerina <Nefedova.Katia@yandex.ru>
2023-04-14 16:43:38 +0400

    Подключение к базе в веб приложении. Хранение вакансий. [475130]

--- src/main/java/ru/job4j/dreamjob/model/City.java
+++ src/main/java/ru/job4j/dreamjob/model/City.java
@@ -1,4 +1,38 @@
 package ru.job4j.dreamjob.model;
 
-public record City(int id, String name) {
+import java.util.Objects;
+
+public class City {
+    private final int id;
+    private final String name;
+
+    public City(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        City city = (City) o;
+        return id == city.id && Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
